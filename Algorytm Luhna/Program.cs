5ebcfb4d8d8a16c8d8af2e3c54444aa4ec471699
Kacper096 <kacpermatuszek2@gmail.com>
2018-10-19 16:19:20 +0200

    Dodaj pliki projektów.

---
+++ Algorytm Luhna/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algorytm_Luhna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
